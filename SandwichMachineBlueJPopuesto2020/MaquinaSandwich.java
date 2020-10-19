2ab1a7459c6a48954f4087e957b29ae0225a400c
Jaider-Cardenas <73125066+Jaider-Cardenas@users.noreply.github.com>
2020-10-19 11:19:32 -0500

    Update MaquinaSandwich.java

--- SandwichMachineBlueJPopuesto2020/MaquinaSandwich.java
+++ SandwichMachineBlueJPopuesto2020/MaquinaSandwich.java
@@ -9,24 +9,17 @@ class MaquinaSandwich{
     static final int CANTIDAD_BASE_QUESO = 3000;
     static final int CANTIDAD_BASE_MORTADELA_JAMON = 1000;
     static final int CANTIDAD_BASE_PAN = 100;
-    static final int cantidadQueso;
-    static final int cantidadMortadela;
-    static final int cantidadJamon;
-    static final int cantidadPanBlanco;
-    static final int cantidadPanIntegral;
-    static final int precioCompraQueso;
-    static final int precioCompraMortadela;
-    static final int precioCompraJamon;
-    static final int precioCompraPanBlanco;
-    static final int precioCompraPanIntegral;
-    static final int ingresosPorVentas;
-    
-    MaquinaSandiwch(int queso, int jamon, int mortadela, int panIntegral, int panBlanco)
-        this.cantidadQueso = queso+CANTIDAD_BASE_QUESO;
-        this.cantidadMortadela=mortadela+CANTIDAD_BASE_MORTADELA_JAMON;
-        this.cantidadJamon=Jamon+CANTIDAD_BASE_MORTADELA_JAMON;
-        this.cantidadPanBlanco=panBlanco+CANTIDAD_BASE_PAN;
-        this.cantidadPanIntegral=panIntegral+CANTIDAD_BASE_PAN;
+     int cantidadQueso;
+     int cantidadMortadela;
+     int cantidadJamon;
+     int cantidadPanBlanco;
+     int cantidadPanIntegral;
+     int precioCompraQueso;
+    int precioCompraMortadela;
+     int precioCompraJamon;
+    int precioCompraPanBlanco;
+    int precioCompraPanIntegral;
+     int ingresosPorVentas;
 
     MaquinaSandwich() {
 
