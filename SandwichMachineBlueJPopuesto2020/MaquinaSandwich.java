160041b54ee6b2b2d207a90b14fef1ed733868a6
Jaider-Cardenas <73125066+Jaider-Cardenas@users.noreply.github.com>
2020-10-19 11:35:25 -0500

    Update MaquinaSandwich.java

--- SandwichMachineBlueJPopuesto2020/MaquinaSandwich.java
+++ SandwichMachineBlueJPopuesto2020/MaquinaSandwich.java
@@ -15,17 +15,16 @@ class MaquinaSandwich{
      int cantidadPanBlanco;
      int cantidadPanIntegral;
      int precioCompraQueso;
-    int precioCompraMortadela;
+     int precioCompraMortadela;
      int precioCompraJamon;
-    int precioCompraPanBlanco;
-    int precioCompraPanIntegral;
+     int precioCompraPanBlanco;
+     int precioCompraPanIntegral;
      int ingresosPorVentas;
 
     MaquinaSandwich() {
 
     }
-
-    MaquinaSandwich(int precioCompraQueso, int precioCompraMortadela, int precioCompraJamon, int precioCompraPanBlanco, int precioCompraPanIntegral) {
+ MaquinaSandwich(int precioCompraQueso, int precioCompraMortadela, int precioCompraJamon, int precioCompraPanBlanco, int precioCompraPanIntegral) {
       //Complete
       this.precioCompraQueso=precioCompraQueso;
       this.precioCompraMortadela=precioCompraMortadela;
@@ -35,17 +34,146 @@ class MaquinaSandwich{
     }
 
     public int calcularPrecioVenta(int tipoSandwich, int tipoPan) {
-      //Complete
+      return 0;
     }
 
     public int calcularCostoFabricacion(int tipoSandwich, int tipoPan) {
-      //Complete
+      return 0;
     }
 
     public boolean registrarVenta(int tipoSandwich, int tipoPan) {
-      //Complete
+      return false;
     }
 
-    //Complete
+    /**Getter method cantidadQueso*/
+    public int getCantidadQueso(){
+        return this.cantidadQueso;
+    }//end method getCantidadQueso
 
+    /**Setter method cantidadQueso*/
+    public void setCantidadQueso(int cantidadQueso){
+        this.cantidadQueso = cantidadQueso;
+    }//end method setCantidadQueso
+
+    /**Getter method cantidadMortadela*/
+    public int getCantidadMortadela(){
+        return this.cantidadMortadela;
+    }//end method getCantidadMortadela
+
+    /**Setter method cantidadMortadela*/
+    public void setCantidadMortadela(int cantidadMortadela){
+        this.cantidadMortadela = cantidadMortadela;
+    }//end method setCantidadMortadela
+
+    /**Getter method cantidadJamon*/
+    public int getCantidadJamon(){
+        return this.cantidadJamon;
+    }//end method getCantidadJamon
+
+    /**Setter method cantidadJamon*/
+    public void setCantidadJamon(int cantidadJamon){
+        this.cantidadJamon = cantidadJamon;
+    }//end method setCantidadJamon
+
+    /**Getter method cantidadPanBlanco*/
+    public int getCantidadPanBlanco(){
+        return this.cantidadPanBlanco;
+    }//end method getCantidadPanBlanco
+
+    /**Setter method cantidadPanBlanco*/
+    public void setCantidadPanBlanco(int cantidadPanBlanco){
+        this.cantidadPanBlanco = cantidadPanBlanco;
+    }//end method setCantidadPanBlanco
+
+    /**Getter method cantidadPanIntegral*/
+    public int getCantidadPanIntegral(){
+        return this.cantidadPanIntegral;
+    }//end method getCantidadPanIntegral
+
+    /**Setter method cantidadPanIntegral*/
+    public void setCantidadPanIntegral(int cantidadPanIntegral){
+        this.cantidadPanIntegral = cantidadPanIntegral;
+    }//end method setCantidadPanIntegral
+
+    /**Getter method precioCompraQueso*/
+    public int getPrecioCompraQueso(){
+        return this.precioCompraQueso;
+    }//end method getPrecioCompraQueso
+
+    /**Setter method precioCompraQueso*/
+    public void setPrecioCompraQueso(int precioCompraQueso){
+        this.precioCompraQueso = precioCompraQueso;
+    }//end method setPrecioCompraQueso
+
+    /**Getter method precioCompraMortadela*/
+    public int getPrecioCompraMortadela(){
+        return this.precioCompraMortadela;
+    }//end method getPrecioCompraMortadela
+
+    /**Setter method precioCompraMortadela*/
+    public void setPrecioCompraMortadela(int precioCompraMortadela){
+        this.precioCompraMortadela = precioCompraMortadela;
+    }//end method setPrecioCompraMortadela
+
+    /**Getter method precioCompraJamon*/
+    public int getPrecioCompraJamon(){
+        return this.precioCompraJamon;
+    }//end method getPrecioCompraJamon
+
+    /**Setter method precioCompraJamon*/
+    public void setPrecioCompraJamon(int precioCompraJamon){
+        this.precioCompraJamon = precioCompraJamon;
+    }//end method setPrecioCompraJamon
+
+    /**Getter method precioCompraPanBlanco*/
+    public int getPrecioCompraPanBlanco(){
+        return this.precioCompraPanBlanco;
+    }//end method getPrecioCompraPanBlanco
+
+    /**Setter method precioCompraPanBlanco*/
+    public void setPrecioCompraPanBlanco(int precioCompraPanBlanco){
+        this.precioCompraPanBlanco = precioCompraPanBlanco;
+    }//end method setPrecioCompraPanBlanco
+
+    /**Getter method precioCompraPanIntegral*/
+    public int getPrecioCompraPanIntegral(){
+        return this.precioCompraPanIntegral;
+    }//end method getPrecioCompraPanIntegral
+
+    /**Setter method precioCompraPanIntegral*/
+    public void setPrecioCompraPanIntegral(int precioCompraPanIntegral){
+        this.precioCompraPanIntegral = precioCompraPanIntegral;
+    }//end method setPrecioCompraPanIntegral
+
+    /**Getter method ingresosPorVentas*/
+    public int getIngresosPorVentas(){
+        return this.ingresosPorVentas;
+    }//end method getIngresosPorVentas
+
+    /**Setter method ingresosPorVentas*/
+    public void setIngresosPorVentas(int ingresosPorVentas){
+        this.ingresosPorVentas = ingresosPorVentas;
+    }//end method setIngresosPorVentas
+    
+    /**Getter method ingresosPorVentas*/
+    public int getIVA(){
+        return 16/100;
+    }//end method getIngresosPorVentas
+
+        /**Getter method ingresosPorVentas*/
+    public int getCostosFijos(){
+        return 0;
+    }//end method getIngresosPorVentas
+    //End GetterSetterExtension Code
+    
+        /**Getter method ingresosPorVentas*/
+    public int getRiesgos(){
+        return 0;
+    }//end method getIngresosPorVentas
+    
+        /**Getter method ingresosPorVentas*/
+    public int getGananciaNeta(){
+        return 0;
+    }//end method getIngresosPorVentas
+//!
 }
